b30f156a607a8e3eabb13ab05ce474ad201cecd7
Nolan <Nolan@10.0.1.10>
2013-03-26 21:44:20 -0700

    Added in the basics for creating a window Need to build back up to the states

--- src/main/client.java
+++ src/main/client.java
@@ -1,10 +1,30 @@
 package main;
 
+import org.lwjgl.opengl.*;
+import org.lwjgl.*;
+import static org.lwjgl.opengl.GL11.*;
+ 
 public class client {
-
 	public static void main(String[] args) {
-		
-
+		try {
+			Display.setDisplayMode(new DisplayMode(640, 480));
+			Display.setTitle("Display");
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			Display.destroy();
+			System.exit(1);
+		}
+		while(!Display.isCloseRequested()) {
+			Display.update();
+			Display.sync(60);
+			glMatrixMode(GL_PROJECTION);
+			glLoadIdentity(); // Resets any previous projection matrices
+			glOrtho(0, 640, 480, 0, 1, -1);
+			glMatrixMode(GL_MODELVIEW);
+		}
+		Display.destroy();
+		System.exit(0);
 	}
 
 }
